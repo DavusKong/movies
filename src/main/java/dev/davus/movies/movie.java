a443a4980b9de1d0a2cc58c4f1aa1ee0703ba5f1
DavusKong <djamieson2005@gmail.com>
2024-04-24 18:52:24 +0100

    Initial import

---
+++ src/main/java/dev/davus/movies/movie.java
@@ -0,0 +1,21 @@
+package dev.davus.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection= "movies")
+public class movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+
+}
