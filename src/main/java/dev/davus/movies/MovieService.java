e053fd72e90d04324e714e26517d51baa68fd783
DavusKong <djamieson2005@gmail.com>
2024-04-24 22:18:20 +0100

    Adding extra api method to search by imdbid

--- src/main/java/dev/davus/movies/MovieService.java
+++ src/main/java/dev/davus/movies/MovieService.java
@@ -1,8 +1,10 @@
 package dev.davus.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -11,5 +13,8 @@ public class MovieService {
 
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+    public Optional<Movie> singleMovie(String imDBId) {
+        return movieRepository.findMovieByImdbId(imDBId);
     }
 }
